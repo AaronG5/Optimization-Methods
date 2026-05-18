238e7e2055f7e2ac537fd5a083855e42f8ad7c7b
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-05-18 23:29:19 +0300

    Initial 4th project commit

---
+++ Project_4/main.py
@@ -0,0 +1,17 @@
+# min (2*x_1 - 3*x_2 - 5*x_4)
+# Apribojimai:
+# 1. -x_1 + x_2 - x_3 - x_4 ≤ 8
+# 2. 2*x_1 + 4*x_2 ≤ 10
+# 3. x_3 + x_4 ≤ 3
+# 4. x_i ≥ 0
+
+# a = 9
+# b = 3
+# c = 4
+
+
+def main():
+   pass
+
+if __name__ == '__main__':
+   main()
